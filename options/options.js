b988052534b5d69f7badf7f2e2bbf78ee0b51689
Crowdbotics <team@crowdbotics.com>
2024-02-22 10:44:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gen-ai-champion-bot-46565",
+  url: "https://gen-ai-champion-bot-46565.botics.co",
+  api: "https://gen-ai-champion-bot-46565.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
