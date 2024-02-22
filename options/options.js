53bbddc801592624dc55f616febe16aa2f0ddbaa
Harry Harry <harry.ault@avanade.com>
2024-02-22 14:38:13 +0000

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "gen-ai-champion-bot-46565",
   url: "https://gen-ai-champion-bot-46565.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
